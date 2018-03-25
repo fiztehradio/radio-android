2eb6e88d0c22bfe76b4e3cf515742c0423f15223
Vadim Matsishin <matsishin.vadim@gmail.com>
2018-03-25 23:30:56 +0300

    beaty added

--- app/src/main/java/ru/mipt/radio/MainActivity.java
+++ app/src/main/java/ru/mipt/radio/MainActivity.java
@@ -1,17 +1,17 @@
 package ru.mipt.radio;
 
 import android.animation.ValueAnimator;
+import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v4.content.LocalBroadcastManager;
-import android.support.v7.app.AppCompatActivity;
 import android.view.animation.LinearInterpolator;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private ImageView playButton;
     private ImageView miptPlanet;
